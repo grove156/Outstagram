b077dd161ca8955e3f442cb1f3e85843f1965368
Maurice <grove156@gmail.com>
2019-12-11 22:28:33 +1100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+      <div class="row">
+          <div class="col-3 pl-5">
+            <img src="https://instagram.fmel5-1.fna.fbcdn.net/v/t51.2885-19/s150x150/51279173_701532740243702_8505887665474764800_n.jpg?_nc_ht=instagram.fmel5-1.fna.fbcdn.net&oh=3a91bcb5983b37acc06e193fb91e9df0&oe=5E741D42" class="rounded-circle">
+          </div>
+        <div class="col-9">
+            <div><h1>Probsno</h1></div>
+            <div class="d-flex">
+                  <div class="pr-5"><strong>63</strong> post</div>
+                  <div class="pr-5"><strong>730k</strong> followers</div>
+                  <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">Myeongkeun Kim</div>
+            <div>This is my page</div>
+            <div>Myeongkeun Kim</div>
+            <div><a href="x">www.myeongkeun.com</div>
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.fmel5-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/62538920_152766465901772_3481100579776209145_n.jpg?_nc_ht=instagram.fmel5-1.fna.fbcdn.net&amp;_nc_cat=108&amp;oh=1a44e3cb75fc732fe108158058668c3b&amp;oe=5EB3DFB3">
+        </div>
+
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.fmel5-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/61975929_368013050518821_8730619967435633345_n.jpg?_nc_ht=instagram.fmel5-1.fna.fbcdn.net&amp;_nc_cat=109&amp;oh=456b06530fd3198820eab793cb684b3d&amp;oe=5E80127B">
+        </div>
+
+        <div class="col-4">
+            <img class="w-100" src="https://instagram.fmel5-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/s640x640/59363933_1661449577332686_8615778018028507424_n.jpg?_nc_ht=instagram.fmel5-1.fna.fbcdn.net&amp;_nc_cat=106&amp;oh=4a2136d0f5d6929eb41637abd849691a&amp;oe=5E7259BE">
+        </div>
+    </div>
+</div>
+@endsection
